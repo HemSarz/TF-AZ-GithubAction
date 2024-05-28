d463121e0697f4a1b8ff64d214a9618ebc600737
gb <gb@hemensarzalihotmail.onmicrosoft.com>
2024-05-28 20:35:15 +0200

    Configure .tf

---
+++ terraform/providers.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  features {
+
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = var.state_resource_group_name
+    storage_account_name = var.state_storage_account_name
+    container_name       = "tfstate"
+    key                  = "terraform-base.tfstate"
+  }
+}
+
