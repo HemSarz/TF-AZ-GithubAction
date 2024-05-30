25c0cc4c232d8418acb32245b01c099a3d693899
gb <gb@hemensarzalihotmail.onmicrosoft.com>
2024-05-30 21:34:56 +0200

    Fix release issues and update configurations

--- gitp.ps1
+++ gitp.ps1
@@ -5,8 +5,8 @@ git add -A
 git commit -m "Fix release issues and update configurations"
 
 # SemanticVersioning
-git tag v1.0.1
+git tag v1.0.2
 
 
 git push origin main
-git push origin v1.0.1
+git push origin v1.0.2
