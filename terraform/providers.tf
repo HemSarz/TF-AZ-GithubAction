b035c41e3cd939daece802c48ff57091bfd21109
gb <gb@hemensarzalihotmail.onmicrosoft.com>
2024-05-30 20:55:13 +0200

    Configure .tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,8 +8,8 @@ terraform {
   backend "azurerm" {
     resource_group_name  = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
-    container_name       = "tfstate"
-    key                  = "terraform-base.tfstate"
+    container_name       = var.state_storage_account_container_name
+    key                  = var.state_key_name
   }
 
   required_providers {
