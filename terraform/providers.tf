8e5d80597cc81bb49cf4eae142a58dad98b96ffe
gb <gb@hemensarzalihotmail.onmicrosoft.com>
2024-05-31 10:21:26 +0200

    Update: yml config

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = var.state_resource_group_name
     storage_account_name = var.state_storage_account_name
-    container_name       = "tfstatebckndcont"
+    container_name       = "tfstatebcknd"
     key                  = "tfstatebcknd"
   }
 
